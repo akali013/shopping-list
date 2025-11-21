b540a6df68b6d450bae1149f2a1f6cc51b56f0c7
akali013 <131390728+akali013@users.noreply.github.com>
2025-11-21 13:21:35 -0500

    Added backend for adding, updating, and deleting list items

--- src/app/_services/items.service.ts
+++ src/app/_services/items.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, Observable, of, tap } from 'rxjs';
-import { ShoppingListItem } from '../_models/shopping-list-item';
 import { environment } from 'src/environments/environment';
+import { Item } from '../_models/item';
 
 @Injectable({
   providedIn: 'root'
@@ -13,39 +13,37 @@ export class ItemsService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  getItems(): Observable<ShoppingListItem[]> {
-    return this.http.get<ShoppingListItem[]>(this.apiUrl)
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.apiUrl)
       .pipe(
         tap(_ => console.log("got list items")),
         catchError(this.handleError("getItems", []))
       );
   }
 
-  addItem(itemName: string): Observable<ShoppingListItem> {
+  createItem(itemName: string): Observable<Item> {
     const item = {
       name: itemName,
-      quantity: 1,
-      isChecked: false
     };
     
-    return this.http.post<ShoppingListItem>(this.apiUrl, item, this.httpOptions)
+    return this.http.post<Item>(this.apiUrl, item, this.httpOptions)
       .pipe(
         tap(_ => console.log("added item"))
       );
   }
 
-  searchItems(searchTerm: string): Observable<ShoppingListItem[]> {
+  searchItems(searchTerm: string): Observable<Item[]> {
     // Check for empty terms
     if (!searchTerm.trim()) { 
       return of([]);
     }
 
-    return this.http.get<ShoppingListItem[]>(`${this.apiUrl}/search/${searchTerm}`).pipe(
+    return this.http.get<Item[]>(`${this.apiUrl}/search/${searchTerm}`).pipe(
       tap(items => items.length ? 
           console.log(`Found items matching "${searchTerm}"`) :
           console.log(`No items matching "${searchTerm}"`)
       ),
-      catchError(this.handleError<ShoppingListItem[]>("searchItems", []))
+      catchError(this.handleError<Item[]>("searchItems", []))
     );
   }
 
--- src/app/_services/shopping-list-item.service.ts
+++ src/app/_services/shopping-list-item.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap, of, catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ShoppingListItem } from '../_models/shopping-list-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListItemService {
+  apiUrl = `${environment.apiUrl}/shoppinglistitem`;
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    withCredentials: true
+  };
 
-  constructor() { }
+  getItems(): Observable<ShoppingListItem[]> {
+    return this.http.get<ShoppingListItem[]>(this.apiUrl, this.httpOptions).pipe(
+      tap(_ => console.log("retrieved items")),
+      catchError(this.handleError("getItems", []))
+    );
+  }
+
+  // Adds the chosen item to the user's shopping list
+  // (The user is identified via the cookie's refresh token)
+  addItem(itemName: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { "Name": itemName, "Quantity": 1 }, this.httpOptions).pipe(
+      tap(item => console.log(`added item: ${item}`)),
+      catchError(this.handleError("addItem", ""))
+    );
+  }
+
+  updateItem(item: ShoppingListItem): Observable<any> {
+    return this.http.put<any>(this.apiUrl, {"Name": item.name, "Quantity": item.quantity, "IsChecked": item.isChecked}, this.httpOptions).pipe(
+      tap(item => console.log(`updated item: ${item}`)),
+      catchError(this.handleError("updateItem", ""))
+    );
+  }
+
+  deleteItem(item: ShoppingListItem): Observable<any> {
+    return this.http.delete(this.apiUrl, {...this.httpOptions, params: {"Name": item.name, "Quantity": item.quantity, "IsChecked": item.isChecked}}).pipe(
+      tap(_ => console.log("Deleted item")),
+      catchError(this.handleError("deleteItem", ""))
+    );
+  }
+
+  handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation}: ${error}`);
+      return of(result as T);
+    }
+  }
+
+  constructor(private http: HttpClient) { }
 }
